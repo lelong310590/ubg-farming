6476835a0d3783af091e53c593cb1aafe07c9768
Long Le Ngoc <longln@tohsoft.com>
2022-03-22 09:03:26 +0700

    update

--- src/pages/home/components/SectionFarming.tsx
+++ src/pages/home/components/SectionFarming.tsx
@@ -453,11 +453,7 @@ const Form: FC = () => {
 													<Fragment>
 														<div className="pool-item-info-row pool-award">
 															<div className="farming-pool-label">Your reward:  </div>
-															{selectedPool.tokenAddress === SmcService.configs.SMC_UBG_TOKEN_ADDRESS ? (
-																<div className="farming-pool-value">{(calculateFarm[0] - totalClaim) / 1e9} UBG</div>
-															) : (
-																<div className="farming-pool-value">{(calculateFarm[0] - totalClaim) / 1e18} UBG</div>
-															)}
+															<div className="farming-pool-value">{(calculateFarm[0] - totalClaim) / 1e9} UBG</div>
 														</div>
 														<div className="pool-item-info-row pool-award">
 															<div className="farming-pool-label">Your deposit: </div>
